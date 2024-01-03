55a45293389b5b204f1d1c19dd74e7e4d4d43b03
MarkeyMarke <casapao.markkenneth@gmail.com>
2024-01-03 00:31:17 -0800

    Added scroll spy and nav

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+function Navigation() {
+    return (
+        <div>
+            <p data-to-scrollspy-id="section-1">Section 1</p>
+            <p data-to-scrollspy-id="section-2">Section 2</p>
+            <p data-to-scrollspy-id="section-3">Section 3</p>
+        </div>
+    );
+}
+
+export default Navigation;
---
+++ src/components/NavigationContent.js
@@ -0,0 +1,25 @@
+import ScrollSpy from 'react-ui-scrollspy';
+
+function NavigationContent() {
+    return (
+        <ScrollSpy>
+            <div id="section-1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolores veritatis doloremque fugit. Soluta
+                aperiam atque inventore deleniti, voluptatibus non fuga eos magni natus vel, rerum excepturi expedita.
+                Tempore, vero!
+            </div>
+            <div id="section-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolores veritatis doloremque fugit. Soluta
+                aperiam atque inventore deleniti, voluptatibus non fuga eos magni natus vel, rerum excepturi expedita.
+                Tempore, vero!
+            </div>
+            <div id="section-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dolores veritatis doloremque fugit. Soluta
+                aperiam atque inventore deleniti, voluptatibus non fuga eos magni natus vel, rerum excepturi expedita.
+                Tempore, vero!
+            </div>
+        </ScrollSpy>
+    );
+}
+
+export default NavigationContent;
