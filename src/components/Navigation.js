001f71b134921891b9236438e11e60d829a0e1a5
MarkeyMarke <casapao.markkenneth@gmail.com>
2024-01-03 16:54:10 -0800

    Test deploy 1

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,10 @@
 function Navigation() {
     return (
         <div>
-            <p data-to-scrollspy-id="section-1">Section 1</p>
-            <p data-to-scrollspy-id="section-2">Section 2</p>
-            <p data-to-scrollspy-id="section-3">Section 3</p>
+            <p data-to-scrollspy-id="section-1">Section One</p>
+            <p data-to-scrollspy-id="section-2">Section Two</p>
+            <p data-to-scrollspy-id="section-3">Section Three</p>
+            <p>Test Again</p>
         </div>
     );
 }
