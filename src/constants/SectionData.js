1f83a75590cb8beb1f74ab3aeb681e62aa245880
MarkeyMarke <casapao.markkenneth@gmail.com>
2024-01-07 19:00:54 -0800

    fix icons on nav

--- src/constants/SectionData.js
+++ src/constants/SectionData.js
@@ -4,8 +4,8 @@ import { faDiceD20 } from '@fortawesome/free-solid-svg-icons/faDiceD20';
 import { faGamepad } from '@fortawesome/free-solid-svg-icons/faGamepad';
 
 export default Object.freeze({
-    WELCOME: { id: 'welcome', label: 'Welcome', icon: faBriefcase },
-    EXPERIENCE: { id: 'experience', label: 'Experience', icon: faUserTie },
+    WELCOME: { id: 'welcome', label: 'Welcome', icon: faUserTie },
+    EXPERIENCE: { id: 'experience', label: 'Experience', icon: faBriefcase },
     DND: { id: 'dnd', label: 'Dungeons & Dragons', icon: faDiceD20 },
     GAME_DEV: { id: 'game-dev', label: 'Game Dev', icon: faGamepad },
 });
