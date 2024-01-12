174110b3b3cf10d37ecf193f60f8c789977f850d
MarkeyMarke <casapao.markkenneth@gmail.com>
2024-01-11 23:21:19 -0800

    Shorter descriptions in videogame section, update nav icon to dragon instead of d20

--- src/constants/SectionData.js
+++ src/constants/SectionData.js
@@ -1,11 +1,11 @@
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons/faBriefcase';
 import { faUserTie } from '@fortawesome/free-solid-svg-icons/faUserTie';
-import { faDiceD20 } from '@fortawesome/free-solid-svg-icons/faDiceD20';
+import { faDragon } from '@fortawesome/free-solid-svg-icons/faDragon';
 import { faGamepad } from '@fortawesome/free-solid-svg-icons/faGamepad';
 
 export default Object.freeze({
     WELCOME: { id: 'welcome', label: 'Welcome', icon: faUserTie },
     EXPERIENCE: { id: 'experience', label: 'Experience', icon: faBriefcase },
-    DND: { id: 'dnd', label: 'Dungeons & Dragons', icon: faDiceD20 },
+    DND: { id: 'dnd', label: 'Dungeons & Dragons', icon: faDragon },
     GAME_DEV: { id: 'game-dev', label: 'Video Games', icon: faGamepad },
 });
