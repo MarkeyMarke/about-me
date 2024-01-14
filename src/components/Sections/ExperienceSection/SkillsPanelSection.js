2548526a38048122d53008858f8bda136d15e146
MarkeyMarke <casapao.markkenneth@gmail.com>
2024-01-14 10:19:02 -0800

    Make custom font awesome icon object for csharp

--- src/components/Sections/ExperienceSection/SkillsPanelSection.js
+++ src/components/Sections/ExperienceSection/SkillsPanelSection.js
@@ -3,23 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import getStringHashCode from '../../../services/getStringHashCode';
 
 const propTypes = {
-    titleIcon: PropTypes.shape({}),
-    titleImage: PropTypes.node,
+    titleIcon: PropTypes.shape({}).isRequired,
     titleLabel: PropTypes.string.isRequired,
     listItems: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
-const defaultProps = {
-    titleImage: undefined,
-    titleIcon: undefined,
-};
-
-function SkillsPanelSection({ titleIcon, titleLabel, listItems, titleImage }) {
+function SkillsPanelSection({ titleIcon, titleLabel, listItems }) {
     return (
         <section className="skills-panel-section">
             <div className="skills-panel-section-title">
-                {titleImage}
-                {titleIcon && <FontAwesomeIcon icon={titleIcon} />}
+                <FontAwesomeIcon icon={titleIcon} />
                 <h1>{titleLabel}</h1>
             </div>
             <div className="skills-panel-section-list-container">
@@ -35,5 +28,4 @@ function SkillsPanelSection({ titleIcon, titleLabel, listItems, titleImage }) {
 }
 
 SkillsPanelSection.propTypes = propTypes;
-SkillsPanelSection.defaultProps = defaultProps;
 export default SkillsPanelSection;
