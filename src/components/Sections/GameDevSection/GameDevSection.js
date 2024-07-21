a408d7b0ebb7137ca6473f20330fe3ebeeb7b8be
MarkeyMarke <casapao.markkenneth@gmail.com>
2024-07-20 20:08:44 -0700

    Update videogame section data

--- src/components/Sections/GameDevSection/GameDevSection.js
+++ src/components/Sections/GameDevSection/GameDevSection.js
@@ -13,22 +13,22 @@ import InlineLink from '../../InlineLink';
 const unityHref = 'https://unity.com/';
 const DESCRIPTION_ITEMS = [
     {
-        term: `Baldur's Gate 3`,
-        id: 'BG3',
-        description: `This game is especially fun when you're playing with friends. There are hundreds of hours worth of content with many significant story choices. Adding the many options for classes and builds the sheer versatility makes this game extremely replayable.`,
-        href: 'https://store.steampowered.com/app/1086940/Baldurs_Gate_3/',
+        term: `Like a Dragon: Infinite Wealth`,
+        id: 'LAD:IW',
+        description: `Hilariously fun adventures and a great story aside, what I really appreciated about this game was that its turn-based combat seamlessly included concepts like guarding, positioning, and quick-time events to keep the gameplay interactive.`,
+        href: 'https://store.steampowered.com/app/2072450/Like_a_Dragon_Infinite_Wealth/',
     },
     {
         term: `Persona 5: Royal`,
         id: 'P5R',
-        description: `The very ambitious approach this game took for the stylish menus, music, and combat stood out to me more than any other game I've ever played. 
-        The character writing is also exceptional, to the point where I enjoyed the dialogue, even if the cutscenes would last several minutes.`,
+        description: `The very ambitious approach this game took for the stylish menus, music, and presentation stood out to me more than any other game I've ever played. 
+        The character writing is also exceptional, to the point where I found every character in the main cast enjoyable.`,
         href: 'https://store.steampowered.com/app/1687950/Persona_5_Royal/',
     },
     {
         term: `Witcher 3: Wild Hunt`,
         id: 'W3',
-        description: `The visuals were incredibly stunning, to the point where most games released in 2023 still can't compare. The music and lore of the game were so well written that it was easy to lose hours of the day immersing myself in the world that the main character lived in.`,
+        description: `The visuals were incredibly stunning, to the point where most games released in 2023 still can't compare. The music and lore of the game were so well written that it was easy to lose hours of the day immersing myself in the world of Witcher.`,
         href: 'https://store.steampowered.com/app/292030/The_Witcher_3_Wild_Hunt/',
     },
     {
@@ -40,14 +40,14 @@ const DESCRIPTION_ITEMS = [
     {
         term: `Cyberpunk 2077`,
         id: 'CP2077',
-        description: `Despite this game's bad reputation for its release, CD Projekt Red has done well to clean up the game ever since, and they managed to turn it around. The game has overhauled the combat system twice now and made it drastically more fun and stylish with each patch.`,
+        description: `Despite this game's bad reputation for its release, CD Projekt Red has done well to clean up the game ever since, and they managed to turn it around. The game has overhauled the combat system twice now and made it drastically more fun with each patch.`,
         href: 'https://store.steampowered.com/app/1091500/Cyberpunk_2077/',
     },
     {
-        term: `Hogwarts Legacy`,
-        id: 'HGL',
-        description: `The player is rewarded for exploration as much as they do for combat. I found something to do in every corner of the game, to the point where it was easy to lose myself in the magical world. On top of that, the combat felt very fluid and fast-paced.`,
-        href: 'https://store.steampowered.com/app/990080/Hogwarts_Legacy/',
+        term: `Ghost of Tsushima`,
+        id: 'GoT',
+        description: `Combining beautiful cinematography and very responsive combat is combined into one game. On one hand, you're immersed into vividly beautiful landscapes that respond to the wind. On the other hand, you're provided a very in-depth combat that really encourages you to use every attack you learn against your enemies.`,
+        href: 'https://store.steampowered.com/app/2215430/Ghost_of_Tsushima_DIRECTORS_CUT/',
     },
 ];
 
